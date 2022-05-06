5ab95f1e2f0c24e804cd2ea316b1a398c6c1d62c
Carlos Yero <cyero002@fiu.edu>
2022-05-06 11:08:42 -0400

    updated

--- cafe_setup.sh
+++ cafe_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-echo"================================================="
+echo "================================================="
 echo "For help on usage, type: ./cafe_setup.sh -help"
-echo"================================================="
+echo "================================================="
 Help()
 {
     # Display Help
@@ -17,37 +17,46 @@ Help()
     echo "options:"
     echo "-help    Print this help display"
     echo ""
-    echo "For users on IFARM: "
+    echo "For users on IFARM ONLY: "
     echo "-f    ONLY use this option if you are running this shell script on ifarm. "
     echo "      This option selects filesystem in in which to read/write data " 
     echo "      from the CaFe experiment. "
     echo ""
-    echo "      The arguments are: test, volatile, work, group"
-    echo "      test: this option (default if no argument is provided) will set" 
-    echo "      up pre-determined raw/ ROOTfiles/ and REPORT_OUTPUT/ symbolic links "
-    echo "      for testing the CaFe replay and analysis scripts using existing data."
+    echo "      The optional arguments are: test, volatile, work, group"
     echo ""
-    echo "      volatile, work, group: these options will set symbolic links to the corresponding filesystem. "
-    echo "      You would want to set these options depending on which stage of the analysis you are in "
-    echo "      for example, select volatile if you are in the beginning stages of off-line analysis."
+    echo "      test: "
+    echo "           this option will set up pre-determined raw/ ROOTfiles/ and REPORT_OUTPUT/ symbolic links" 
+    echo "           for testing the CaFe replay and analysis scripts using existing data."
+    echo ""
+    echo "      volatile, work, group: "
+    echo "            these options will set symbolic links to the corresponding filesystem "
+    echo "            you would want to set these options depending on which stage of the analysis you are in "
+    echo "            for example, use volatile if you are in the beginning stages of off-line analysis"
     echo ""
     echo "      See https://hallcweb.jlab.org/wiki/index.php/CaFe_Disk_Space " 
     echo "      for detailed information on each of these filesystems."  
     echo "        "
-    echo "example: ./cafe_setup.sh -f volatile"
+    echo "examples: ./cafe_setup.sh -f test"
+    echo "          ./cafe_setup.sh -f volatile"
+    echo "          ./cafe_setup.sh -f work" 
+    echo "          ./cafe_setup.sh -f group"   
     echo "-------------------------------------------------------"    
 }
 
 set_hcana_link()
 {
     if [[ -z $HCANALYZER ]]; then	
+	echo ""
 	echo "Environment variable: $HCANALYZER does NOT exist. "
 	echo "Please make sure to do: source setup.sh(csh) in hcana first. " 
+	echo ""
     else
+	echo ""
 	echo "Creating hcana symbolic link now  . . ."
 	ln -sf $HCANALYZER"/hcana"
 	ln -sf $HCANALYZER"/libHallC.so"
 	ln -sf $HCANALYZER"/libHallC.so.0.90.0"
+	echo ""
     fi    
 }
 
@@ -110,14 +119,29 @@ cache_analysis_out="/cache/hallc/c-cafe-2022/analysis/"
 #
 # =================================
 if [[ ifarm_flg -eq 1 ]]; then
-
+    
+    if [[ -z $fsys ]]; then
+	echo ""
+	echo " No optional argumnets provided "
+	echo ""
+	echo "----------------------------------------------------------------------"
+	echo " For help using additional options, please run: ./cafe_setup.sh -help "
+	echo "----------------------------------------------------------------------"
+	echo "Exiting NOW . . . "
+	echo ""
+	exit 1
+    fi
+    
+    echo ""
     echo "Checking if necessary directories or symlinks exist in remote machine: " ${USER}"@"${HOSTNAME}". . ."
-
+    echo ""
+    
     # setup the symbolic links to hcana
     set_hcana_link      
    
     
     if [[ $fsys == "volatile" ]]; then	     
+	echo ""
 	echo 'Setting up symbolic links to volatile filesystem on ifarm . . .'
 	base_dir_voli="/volatile/hallc/c-cafe-2022/"	
 
@@ -137,9 +161,10 @@ if [[ ifarm_flg -eq 1 ]]; then
 	echo "Creating dir and symlink to $base_dir_voli$USER/ROOTfiles . . ."
 	mkdir $base_dir_voli$USER"/ROOTfiles"
 	ln -sf $base_dir_voli$USER"/ROOTfiles"
-	
+	echo ""
 	
     elif [[ $fsys == "work" ]]; then	     
+	echo ""
 	echo 'Setting up symbolic links to work filesystem on ifarm . . .'
 	base_dir_work="/work/hallc/c-cafe-2022/"
 
@@ -181,8 +206,9 @@ if [[ ifarm_flg -eq 1 ]]; then
 	echo "Creating dir and symlink to $base_dir_group$USER/ROOTfiles . . ."
 	mkdir $base_dir_group$USER"/ROOTfiles"
 	ln -sf $base_dir_group$USER"/ROOTfiles"
-	
+	echo ""
     elif [[ $fsys == "test" ]]; then
+	echo ""
 	echo 'Setting up test symlinks on ifarm for testing cafe replay scripts . . .'
 	base_dir="/lustre19/expphy/volatile/hallc/c-cafe-2022/"
 	raw_dir=$base_dir'test_raw'  # this is read-only for users (since dir/ was created by cyero to put raw test files)
@@ -199,32 +225,7 @@ if [[ ifarm_flg -eq 1 ]]; then
 	ln -sf $raw_dir raw	
 	ln -sf $ROOTfiles_dir
 	ln -sf $REPORT_OUTPUT_dir
-
-    elif [[ -z $fsys ]]; then
-	echo "No optional argumnet provided. Will default to setting up the symbolic links "
-	echo "  to the user directory in volatile for testing cafe replay scripts . . ."
 	echo ""
-	echo "----------------------------------------------------------------------"
-	echo " For help using additional options, please run: ./cafe_setup.sh -help "
-	echo "----------------------------------------------------------------------"
-
-	base_dir="/lustre19/expphy/volatile/hallc/c-cafe-2022/"
-	raw_dir=$base_dir'test_raw'  # this is read-only for users (since dir/ was created by cyero to put raw test files)
-
-	base_dir_user="${base_dir}test_output_${USER}/"
-	ROOTfiles_dir=${base_dir_user}"ROOTfiles"
-	REPORT_OUTPUT_dir=$base_dir_user"REPORT_OUTPUT"
-
-	mkdir $base_dir_user
-	mkdir $ROOTfiles_dir
-	mkdir $REPORT_OUTPUT_dir
-
-	unlink raw
-	ln -sf $raw_dir raw	
-	ln -sf $ROOTfiles_dir
-	ln -sf $REPORT_OUTPUT_dir
-
-	
     fi
 fi
 
