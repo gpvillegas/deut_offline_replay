692c7a66ef71855f48cad0302b69c5581ee2a597
Carlos Yero <cyero@jlab.org>
2022-06-24 09:42:54 -0400

    added line to add plot to chars studio

--- UTILS_CAFE/online_scripts/plot_runlist.py
+++ UTILS_CAFE/online_scripts/plot_runlist.py
@@ -3,7 +3,18 @@ import plotly_express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import datetime
- 
+import chart_studio
+import chart_studio.plotly as charts
+
+
+#The initialization step places a special .plotly/.credentials file in your home directory. ( i think only done once)
+#chart_studio.tools.set_credentials_file(username='cyero', api_key='JCKnODSC8EtxOT1GdtND')
+
+
+
+
+
+
 # using now() to get current time
 current_time = datetime.datetime.now()
 print(current_time)
@@ -28,36 +39,42 @@ fig.update_layout(bargap=0)
 
 #fig.add_trace(px.bar(df, x='run', y='charge', color='target', pattern_shape="kin_type").data[1])
 #fig.update_layout(xaxis={'visible': False, 'showticklabels': False})
-fig.show()
+#fig.show()
 
-'''
-how to convert string time_stamp to python formatted timestamp to plot
-from datetime import datetime, timedelta
 
-time_stamp= 'Sat Jun 18 13:13:41 2022'
+charts.plot(fig, filename = 'basic-line', auto_open=True)
 
-In [12]: time_stamp_fmt = datetime.strptime(time_stamp, '%a %b %d %H:%M:%S %Y') make sure to put the correct format of the string
 
-In [13]: time_stamp_fmt
-Out[13]: datetime.datetime(2022, 6, 18, 13, 13, 41)
 
-time_stamp_fmt.timestamp() --> this gives an absolute time number in seconds.
+
+#------------------------
+#how to convert string time_stamp to python formatted timestamp to plot
+#from datetime import datetime, timedelta
+
+#time_stamp= 'Sat Jun 18 13:13:41 2022'
+
+#In [12]: time_stamp_fmt = datetime.strptime(time_stamp, '%a %b %d %H:%M:%S %Y') make sure to put the correct format of the string
+
+#In [13]: time_stamp_fmt
+#Out[13]: datetime.datetime(2022, 6, 18, 13, 13, 41)
+
+#time_stamp_fmt.timestamp() --> this gives an absolute time number in seconds.
 
 # to add delta time (e.g., run length in seconds) to get the end_time, do:
-end_time = time_stamp_fmt + timedelta(seconds=3600)  # if a run lasted 1 hour
+#end_time = time_stamp_fmt + timedelta(seconds=3600)  # if a run lasted 1 hour
 
-In [24]: time_stamp_fmt
-Out[24]: datetime.datetime(2022, 6, 18, 13, 13, 41)
+#In [24]: time_stamp_fmt
+#Out[24]: datetime.datetime(2022, 6, 18, 13, 13, 41)
 
-In [25]: end_time
-Out[25]: datetime.datetime(2022, 6, 22, 2, 13, 41)
+#In [25]: end_time
+#Out[25]: datetime.datetime(2022, 6, 22, 2, 13, 41)
 
 # calculate time width of the run (sec)
-In [26]: end_time.timestamp() - time_stamp_fmt.timestamp()
-Out[26]: 306000.0
+#In [26]: end_time.timestamp() - time_stamp_fmt.timestamp()
+#Out[26]: 306000.0
 
 # calculate mid-point of the run (to put correct tick in center)
-In [27]: time_stamp_fmt + 0.5*(end_time - time_stamp_fmt)
-Out[27]: datetime.datetime(2022, 6, 20, 7, 43, 41)
+#In [27]: time_stamp_fmt + 0.5*(end_time - time_stamp_fmt)
+#Out[27]: datetime.datetime(2022, 6, 20, 7, 43, 41)
 
-'''
+#------------------------
