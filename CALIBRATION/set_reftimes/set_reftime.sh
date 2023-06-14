681e90f410adaf63db6a158aab503f41b836c932
Gema P Villegas <gvill085@fiu.edu>
2023-06-14 09:40:37 -0400

    modified paths to run CALIB script locally

--- CALIBRATION/set_reftimes/set_reftime.sh
+++ CALIBRATION/set_reftimes/set_reftime.sh
@@ -9,7 +9,7 @@ if [ -z "$1" ] || [ -z "$2" ]; then
     echo "" 
     echo ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:"
     echo "" 
-    echo "Usage:  ./set_reftime.sh <run_number> <evt_number>"  
+    echo "Usage:  ./set_reftime.sh <run_number> <evt_number> <replay(optional)>"  
     echo "" 
     echo ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:"
     
@@ -23,18 +23,21 @@ debug=0
 # Which analysis file type are we doing? "reftime" or "timewin"? 
 ana_type="reftime"
 
-# this rootfile name pattern assumes pattern defined in replay_deut.C script (please do NOT modify replay script) 
-filename="../../ROOTfiles/${ana_type}/deut_replay_${ana_type}_${runNum}_${evtNum}.root"
+analysis_home="/home/gvill/deuteron/deut_offline_replay"
 
-replay_script="SCRIPTS/COIN/PRODUCTION/replay_deut.C"
-analysis_script="scripts/set_reftimes.C"
+# this rootfile name pattern assumes pattern defined in replay_deut.C script (please do NOT modify replay script) 
+filename="${analysis_home}/ROOTfiles/${ana_type}/deut_replay_${ana_type}_${runNum}_${evtNum}.root"
+
+replay_script="${analysis_home}/SCRIPTS/COIN/PRODUCTION/replay_deut.C"
+analysis_script="${analysis_home}/CALIBRATION/set_reftimes/scripts/set_reftimes.C"
 
 runHcana="./hcana -q \"${replay_script}(${runNum}, ${evtNum}, \\\"${ana_type}\\\")\""
 runAna="root -l -q -b \"${analysis_script}(\\\"${filename}\\\", ${runNum}, \\\"${daq_mode}\\\", ${set_refTimes}, ${debug})\""  
 
-# change to top direcotry and run analyzer to produce specified ROOTfile
+# change to top directory and run analyzer to produce specified ROOTfile
 if [ "${replay}" == "replay" ]; then 
-    cd ../../
+    cd ${analysis_home}
+    echo running:$runHcana
     eval $runHcana
     # change back to original directory and execute analysis script
     cd CALIBRATION/set_reftimes
