2bf155cc5821398c09fdc1283858519ef172e6c3
Gema P Villegas <gvill085@fiu.edu>
2024-03-06 16:16:17 -0500

    revert changes from -(modified paths to run CALIB script locally)

--- CALIBRATION/set_reftimes/set_reftime.sh
+++ CALIBRATION/set_reftimes/set_reftime.sh
@@ -11,7 +11,11 @@ if [ -z "$1" ] || [ -z "$2" ]; then
     echo "" 
     echo ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=::=:=:"
     echo "" 
+<<<<<<< HEAD
     echo "Usage:  ./set_reftime.sh <run_number> <evt_number> <replay(opt)> <singles?(opt)>"  
+=======
+    echo "Usage:  ./set_reftime.sh <run_number> <evt_number>"  
+>>>>>>> parent of 681e90f (modified paths to run CALIB script locally)
     echo "" 
     echo ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=::=:=:"
     
@@ -38,22 +42,24 @@ fi
 # Which analysis file type are we doing? "reftime" or "timewin"?
 ana_type="reftime"
 
+<<<<<<< HEAD
 # Set analysis directory to local directory
 analysis_home="/home/gvill/deuteron/deut_offline_replay"
 
+=======
+>>>>>>> parent of 681e90f (modified paths to run CALIB script locally)
 # this rootfile name pattern assumes pattern defined in replay_deut.C script (please do NOT modify replay script) 
-filename="${analysis_home}/ROOTfiles/${ana_type}/deut_replay_${ana_type}_${runNum}_${evtNum}.root"
+filename="../../ROOTfiles/${ana_type}/deut_replay_${ana_type}_${runNum}_${evtNum}.root"
 
-replay_script="${analysis_home}/SCRIPTS/COIN/PRODUCTION/replay_deut.C"
-analysis_script="${analysis_home}/CALIBRATION/set_reftimes/scripts/set_reftimes.C"
+replay_script="SCRIPTS/COIN/PRODUCTION/replay_deut.C"
+analysis_script="scripts/set_reftimes.C"
 
 runHcana="./hcana -q \"${replay_script}(${runNum}, ${evtNum}, \\\"${ana_type}\\\")\""
 runAna="root -l -q -b \"${analysis_script}(\\\"${filename}\\\", ${runNum}, \\\"${daq_mode}\\\", ${set_refTimes}, ${debug},${singles})\""  
 
-# change to top directory and run analyzer to produce specified ROOTfile
+# change to top direcotry and run analyzer to produce specified ROOTfile
 if [ "${replay}" == "replay" ]; then 
-    cd ${analysis_home}
-    echo running:$runHcana
+    cd ../../
     eval $runHcana
     # change back to original directory and execute analysis script
     cd CALIBRATION/set_reftimes
