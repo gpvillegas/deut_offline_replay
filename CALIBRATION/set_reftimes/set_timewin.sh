0828795b115a533595a9730b957065a20ead51c1
Gema P Villegas <98612584+gpvillegas@users.noreply.github.com>
2024-03-07 21:48:15 -0500

    Update set_timewin.sh
    undid change to local path

--- CALIBRATION/set_reftimes/set_timewin.sh
+++ CALIBRATION/set_reftimes/set_timewin.sh
@@ -24,7 +24,7 @@ debug=1
 ana_type="timewin"
 
 # Set analysis directory to local directory
-analysis_home="/home/gvill/deuteron/deut_offline_replay"
+analysis_home="../.."
 
 # this rootfile name pattern assumes pattern defined in replay_deut.C script (please do NOT modify replay script) 
 filename="${analysis_home}/ROOTfiles/${ana_type}/deut_replay_${ana_type}_${runNum}_${evtNum}.root"
