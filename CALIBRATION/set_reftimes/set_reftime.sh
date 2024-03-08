9febf23146e7894c45fdc72814a4bea5831f5b2e
Gema P Villegas <98612584+gpvillegas@users.noreply.github.com>
2024-03-08 07:15:03 -0500

    Revert "Develop: general updates from my work branch gema_work"

--- CALIBRATION/set_reftimes/set_reftime.sh
+++ CALIBRATION/set_reftimes/set_reftime.sh
@@ -3,17 +3,15 @@
 #user input
 runNum=$1    # run number
 evtNum=$2    # event number
-replay=$3    # replay data (default no replay)
-singles=$4   # is it a singles run?   
-#daq_mode=$4  # shms(singles) or coin (default coin) always coin for deut23
+replay=$3
 
 if [ -z "$1" ] || [ -z "$2" ]; then 
     echo "" 
-    echo ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=::=:=:"
+    echo ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:"
     echo "" 
-    echo "Usage:  ./set_reftime.sh <run_number> <evt_number> <replay(opt)> <singles?(opt)>"  
+    echo "Usage:  ./set_reftime.sh <run_number> <evt_number>"  
     echo "" 
-    echo ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=::=:=:"
+    echo ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:"
     
     exit 0
 fi
@@ -21,21 +19,8 @@ fi
 daq_mode="coin"
 set_refTimes=1
 debug=0
-#singles=1
-
-if [ -z "$4" ]; then
-    singles=0
-    echo ""
-    echo "Analyzing coincidence run ${runNum}"
-    echo ""
-else
-    singles=1
-    echo ""
-    echo "Analyzing singles run ${runNum}"
-    echo ""
-fi        
-
-# Which analysis file type are we doing? "reftime" or "timewin"?
+  
+# Which analysis file type are we doing? "reftime" or "timewin"? 
 ana_type="reftime"
 
 # this rootfile name pattern assumes pattern defined in replay_deut.C script (please do NOT modify replay script) 
@@ -45,7 +30,7 @@ replay_script="SCRIPTS/COIN/PRODUCTION/replay_deut.C"
 analysis_script="scripts/set_reftimes.C"
 
 runHcana="./hcana -q \"${replay_script}(${runNum}, ${evtNum}, \\\"${ana_type}\\\")\""
-runAna="root -l -q -b \"${analysis_script}(\\\"${filename}\\\", ${runNum}, \\\"${daq_mode}\\\", ${set_refTimes}, ${debug},${singles})\""  
+runAna="root -l -q -b \"${analysis_script}(\\\"${filename}\\\", ${runNum}, \\\"${daq_mode}\\\", ${set_refTimes}, ${debug})\""  
 
 # change to top direcotry and run analyzer to produce specified ROOTfile
 if [ "${replay}" == "replay" ]; then 
