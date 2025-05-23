4a79a765f0ea2c9c0b66cd5dc4fd319810ac02cc
gpvillegas <gvillega@fiu.edu>
2025-05-23 14:45:50 -0400

    work on optics optimization, added optics matrices, other minor changes

--- jcacheGet.py
+++ jcacheGet.py
@@ -1,14 +1,25 @@
 import os
 
-run_nums = open("deut_offline_replay/run-list.dat","r")
+#run_nums = open("./run-list.dat","r")
+#run_nums = [3283,3284,3285,3286,3287]
+run_nums = [20840,20841,20846,20851,20858,20861,20868,20869]
 spec = 'shms'       # shms, hms, or coin
+exp = 'c-deuteron'  # c-deuteron, c-cafe-2022, spring17 (deuteron 2018), ...
 
-
+i = 0
 for run in run_nums:
     r = int(run)
-    tapeDIR = f'/mss/hallc/c-deuteron/raw/{spec}_all_{r}.dat'
-    jcacheCMD = f'jcache get {tapeDIR} -e gvill@jlab.org -D 60'
+    tapeDIR = f'/mss/hallc/{exp}/raw/{spec}_all_{r}.dat'   
 
+    if i == 0:
+        jcacheCMD = f'jcache get {tapeDIR} -e gvill@jlab.org -D 60'
+        i+=1
+    elif i == (len(run_nums)-1):
+        jcacheCMD = f'jcache get {tapeDIR} -e gvill@jlab.org -D 60'
+    else:
+        jcacheCMD = f'jcache get {tapeDIR} -D 60'
+        i+=1    
+    
     os.system(jcacheCMD)
-run_nums.close()
+#run_nums.close()
 
